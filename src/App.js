f49ab5f54fea92ec12ccf9a85ddb780c36ebc932
César Oyarzo Muñoz <84143087+tinguinito@users.noreply.github.com>
2021-06-10 20:50:02 -0400

    test en clase

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Welcome from './components/Welcome.js'
-import Button from './components/Button';
-import TableProducts from './components/TableProducts'
-
+import React from 'react';
 function App() {
   return (
     <div className="App">
@@ -25,14 +22,55 @@ function App() {
 
         </div>
       </div>
-      <TableProducts />
+      <footer>
+        <Clock />
+      </footer>
     </div>
   );
 }
 
 export default App;
 
+function Welcome(props) {
+  return <h1 className="text-primary text-center py-5">Bienvenido a, {props.name}</h1>
+}
 
+function Button(props) {
+  return (
+    <div className="col-3 text-center">
+      <button className={props.nameClass}>{props.label}</button>
+    </div>
+  );
+}
 
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
 
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
 
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}
